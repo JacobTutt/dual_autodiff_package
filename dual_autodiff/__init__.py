045769b21d103163712ea57dc481219cad5e6643
Jacob Tutt <jacobtutt@icloud.com>
2024-11-18 20:04:36 +0000

    Simplify dual_autodiff __init__

--- dual_autodiff/__init__.py
+++ dual_autodiff/__init__.py
@@ -1,28 +1,13 @@
 # dual_autdiff/__init__.py
 
 # Import the main Company class for easy access
-from .dual import Dual
+from .dual import *
 
 # Import version information for the package
 from .version import __version__ 
 
 from .dual import Dual
-from .autodiff_tools import (
-    sin,
-    cos,
-    tan,
-    asin,
-    acos,
-    atan,
-    sinh,
-    cosh,
-    tanh,
-    exp,
-    log,
-    sqrt,
-    pow,
-    auto_diff,
-)
+from .autodiff_tools import *
 
 __all__ = [
     "Dual",
