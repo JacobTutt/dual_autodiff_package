9eb5a5faa0d8300dc6cfa1e201468463bb1b93a9
Jacob Tutt <jacobtutt@icloud.com>
2024-11-20 20:59:34 +0000

    Add documentation URL

--- setup.py
+++ setup.py
@@ -1,14 +1,39 @@
 from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
+import os
+import shutil
+from pathlib import Path
 
+# Function to copy all .py files from a source directory to the cython directory
+# Exclude the __init.__.py and version.py files
+def prepare_cython_sources(source_dir, cython_dir):
+    Path(cython_dir).mkdir(parents=True, exist_ok=True)
+
+    for py_file in Path(source_dir).rglob("*.py"):
+        relative_path = py_file.relative_to(source_dir)
+
+        if relative_path.name in ["__init__.py", "version.py"]:
+            continue
+
+        # Rename .py to .pyx and place in the Cython directory
+        cython_file = Path(cython_dir) / relative_path.with_suffix(".pyx")
+        cython_file.parent.mkdir(parents=True, exist_ok=True)  # Ensure the subdirectories exist
+        shutil.copy(py_file, cython_file)
+
+# execute the copy from dual_autodiff to dual_autodiff_x
+prepare_cython_sources('dual_autodiff', 'dual_autodiff_x')
+
+# Defines the Cython extensions dynamically
 extensions = [
-    Extension("dual_autodiff_x.dual", ["dual_autodiff_x/dual.pyx"]),
-    Extension("dual_autodiff_x.autodiff_tools", ["dual_autodiff_x/autodiff_tools.pyx"]),
+    Extension(
+        name=f"dual_autodiff_x.{pyx_file.stem}",
+        sources=[str(pyx_file)],
+    )
+    for pyx_file in Path("dual_autodiff_x").rglob("*.pyx")
 ]
 
 setup(
-    name="dual_autodiff",  # Jacob make sure it the same as in pyproject.toml
-    version="0.1.0",
+    name="dual_autodiff", 
     description="A Python package for forward-mode automatic differentiation using dual numbers with a Cythonized version for enhanced performance",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -21,11 +46,6 @@ setup(
         compiler_directives={"language_level": "3"}
     ),
     install_requires=[],
-    setup_requires=[
-        "setuptools",
-        "wheel",
-        "Cython",
-    ],
     classifiers = [
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
